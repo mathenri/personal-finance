0336aca53f16605e92ef32cd770064daa670a554
Mattias Henriksson <henriksson.mj@gmail.com>
2018-03-03 17:27:29 +0100

    Refactor: Add new component ExpenseTypeCheckbox

---
+++ client/src/ExpenseTypeCheckbox.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class ExpenseTypeCheckbox extends Component {
+  render() {
+    return (
+      <div className="form-check form-control-lg">
+        <input 
+          type="radio" 
+          className="form-check-input" 
+          name="selectedExpenseType" 
+          id={this.props.formControlValue + 'Radio'} 
+          value={this.props.radioButtonValue}
+          checked={this.props.selectedExpenseType === this.props.radioButtonValue}
+          onChange={this.props.handleFormChange} />
+        <label class="form-check-label" for={this.props.radioButtonValue + 'Radio'}>{this.props.label}</label>
+      </div>
+    );
+  }
+}
+
+export default ExpenseTypeCheckbox;
